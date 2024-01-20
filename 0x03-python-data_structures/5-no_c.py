00bc7e35a2702c7a227cd8a95e96930adadbd286
Fayrouz20 <aitabdelaalifayrouz01@gmail.com>
2024-01-20 15:31:35 -0800

    6. Lists of lists = Matrix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
     my_new_string = ""
+
     for item in range(len(my_string)):
-        if my_sring[item] != "c" or my_string[item] != "C":
+        if (my_sring[item] != "c" or my_string[item] != "C"):
             my_new_string += my_string[i]
 
     return my_new_string
