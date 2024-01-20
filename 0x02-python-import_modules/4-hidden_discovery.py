00ed9a0b6500b051acec23c06899304400d0f6d4
Fayrouz20 <aitabdelaalifayrouz01@gmail.com>
2024-01-20 09:43:01 -0800

    4. Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    #print all hiden directories"
+    # print all hiden directories"
     import hidden_4
     for i in sorted(dir(hidden_4)):
         if i[:2] != "__":
