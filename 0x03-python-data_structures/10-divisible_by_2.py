60edb084f41f9f08b242fb05559c06c5be466976
Fayrouz20 <aitabdelaalifayrouz01@gmail.com>
2024-01-22 17:48:27 -0800

    my 10 bool value

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    my_bool_list = my_list[:]
-    for count, i in enumerate(my_list):
-        if i % 2 == 0:
-            my_bool_list[count] = True
-        else:
-            my_bool_list[count] = False
-    return(my_bool_list)
+    my_bool_list = my_list[]
+    if my_list:
+        for num in my_list:
+            my_bool_list.append(False if num % 2 else True)
+        return my_bool_list
